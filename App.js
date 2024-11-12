5e9175b18c097924747453f876f17ce7f817aaca
Moises Kalebe Medeiros Farias <moises-dev@Moisess-MacBook-Air.local>
2024-11-12 18:59:39 -0300

    feat: fetchs and actions v1

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {StyleSheet, Text, View, TouchableOpacity, TextInput, ActivityIndicator} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity, TextInput, ActivityIndicator, Keyboard} from 'react-native';
 import {EconomiaCurrencyService} from "./src/Services/EconomiaCurrencyService";
 import PickerCurrency from "./src/Components/PickerCurrency";
 
@@ -7,12 +7,14 @@ export default function App() {
   const [loading, setLoading] = useState(true);
   const [coinList, setCoinList] = useState([]);
   const [selectedCoin, setSelectedCoin] = useState([]);
-  const [amountMoneyRaw, setAmountMoneyRaw] = useState('0');
+  const [amountMoneyRaw, setAmountMoneyRaw] = useState('R$ 0,00');
+  const [convertedAmount, setConvertedAmount] = useState('R$ 0,00');
 
   useEffect(() => {
     async function getCoinList() {
       const arrayKeys = [];
       const response = await EconomiaCurrencyService.get('all');
+
       Object.keys(response.data).map((key) => {
         arrayKeys.push({
           key: key,
@@ -43,6 +45,25 @@ export default function App() {
     return number.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
   }
 
+  async function calculateAmountCurrency() {
+    Keyboard.dismiss();
+
+    if (amountMoneyRaw === 'R$ 0,00') {
+      return;
+    }
+
+    const response = await EconomiaCurrencyService.get(`all/${selectedCoin}-BRL`);
+
+    let valorLimpo = amountMoneyRaw.replace('R$', '').trim();
+
+    valorLimpo = valorLimpo.replace(',', '.');
+
+    const number = parseFloat(valorLimpo);
+    const currentCurrencyAmount = (response.data[selectedCoin].ask * number);
+
+    setConvertedAmount(convertStringInReal(currentCurrencyAmount.toFixed(2)));
+  }
+
   return (
       loading ?
           <View style={{ flex:1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'black' }} >
@@ -52,7 +73,10 @@ export default function App() {
       <View style={styles.currencyPickerSection}>
         <Text style={styles.currencyPickerTitle}>Selecione sua moeda</Text>
 
-        <PickerCurrency coinList={coinList} selectedCoin={selectedCoin} onChange={(coin) => setSelectedCoin(coin)}/>
+        <PickerCurrency coinList={coinList} selectedCoin={selectedCoin} onChange={(coin) => {
+          setConvertedAmount('R$ 0,00');
+          setSelectedCoin(coin);
+        }}/>
 
       </View>
 
@@ -62,14 +86,25 @@ export default function App() {
             keyboardType={'numeric'}
             style={styles.amountCurrencyInput}
             placeholder={'ex.: 1.50'}
-            onChangeText={(v) => setAmountMoneyRaw(convertStringInReal(v))}
-            value={amountMoneyRaw}
+            onChangeText={(v) => {
+              setConvertedAmount('R$ 0,00');
+              setAmountMoneyRaw(v)
+            }}
         />
       </View>
 
-      <TouchableOpacity style={styles.buttonArea}>
+      <TouchableOpacity style={styles.buttonArea} onPress={() => calculateAmountCurrency()}>
         <Text style={styles.buttonText}>Converter</Text>
       </TouchableOpacity>
+
+      {convertedAmount !== 'R$ 0,00' ?
+          <View style={styles.resultArea}>
+            <Text style={styles.selectedCoin}>{amountMoneyRaw.replace('R$', '').trim()} {selectedCoin}</Text>
+            <Text style={styles.fixedText}>corresponde á</Text>
+            <Text style={styles.selectedCoin}>{convertedAmount}</Text>
+          </View> :
+          null}
+
     </View>
   );
 }
@@ -120,5 +155,22 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 25,
     fontWeight: "bold"
+  },
+  resultArea: {
+    width: '90%',
+    backgroundColor: 'white',
+    alignItems: 'center',
+    padding: 30,
+    marginTop: 30,
+    borderRadius: 8,
+    gap: 10
+  },
+  selectedCoin: {
+    fontSize: 28,
+    color: '000',
+    fontWeight: 'bold'
+  },
+  fixedText: {
+    fontSize: 18
   }
 });
